111a278ea35fee7dc93c476ed72f697c8cf6878b
Timur <91536915+123alex12@users.noreply.github.com>
2022-10-05 20:57:35 +0300

    Add files via upload

---
+++ online-zoo.week-3/assets/script.js
@@ -0,0 +1,155 @@
+let items = document.querySelectorAll('.item');
+let currentItem = 0;
+let isEnabled = true;
+
+function changeCurrentItem(n) {
+    currentItem = (n + items.length) % items.length;
+}
+
+function hideItem(direction) {
+    isEnabled = false;
+    items[currentItem].classList.add(direction)
+    items[currentItem].addEventListener('animationend', function(){
+        this.classList.remove('active', direction)
+    });
+}
+
+function showItem(direction) {
+    items[currentItem].classList.add('next', direction);
+    items[currentItem].addEventListener('animationend', function(){
+        this.classList.remove('next', direction);
+        this.classList.add('active');
+        isEnabled = true;
+    });
+}
+
+
+
+function previousItem(n) {
+    hideItem('to-right');
+    changeCurrentItem(n - 1);
+    showItem('from-left');
+}
+
+
+function nextItem(n) {
+    hideItem('to-left');
+    changeCurrentItem(n - 1);
+    showItem('from-right');
+}
+
+
+document.querySelector('.control.left').addEventListener('click', function(){
+    if (isEnabled) {
+        previousItem(currentItem)
+    }
+});
+
+
+function previousItem(n) {
+    changeCurrentItem(n - 1)
+}
+
+document.querySelector('.control.right').addEventListener('click', function(){
+    if (isEnabled) {
+        nextItem(currentItem)
+    }
+});
+
+const swipedetect = (el) => {
+    let surface = el;
+    let startX = 0;
+    let startY = 0;
+    let distX = 0;
+    let distY = 0;
+    
+
+    let startTime = 0;
+    let elapsedTime = 0;
+
+    let threshold = 150;
+    let restraint = 100;
+    let allowedTime = 300;
+
+     surface.addEventListener('mousedown', function(e){
+        startX = e.pageX;
+        startY = e.pageY;
+        startTime = new Date().getTime();
+        e.preventDefault();
+     });   
+    
+     surface.addEventListener('mouseup', function(e){
+        distX = e.pageX - startX;
+        distY = e.pageY - startY;
+        elapsedTime = new Date().getTime() - startTime;
+        
+        if(elapsedTime <= allowedTime){
+            if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint){
+                if (distX > 0) {
+                    if (isEnabled) {
+                        previousItem(currentItem);
+                    }
+                } else {
+                    {
+                        if (isEnabled) {
+                            nextItem(currentItem);
+                    }
+                } 
+            }
+        }
+
+        e.preventDefault();
+     });
+
+     surface.addEventListener('touchstart', function(e){
+        if (e.target.classList.contains('arrow') || e.target.classList.contains('control')) {
+            if (e.target.classList.contains('left')) {
+                if (isEnabled) {
+                    nextItem(currentItem);
+                }
+            } else if (e.target.classList.contains('right')) {
+                if (isEnabled) {
+                    nextItem(currentItem);
+                }
+            }
+        }
+        
+        let touchObj = e.changedTouches[0];
+        startX = touchObj.pageX;
+        startY = touchObj.pageY;
+        startTime = new Date().getTime();
+        e.preventDefault();
+     });   
+    
+     surface.addEventListener('touchmove', function(e){
+        e.preventDefault();
+     });   
+     
+
+     surface.addEventListener('touchend', function(e){
+        let touchObj = e.changedTouches[0];
+        distX = touchObj.pageX - startX;
+        distY = touchObj.pageY - startY;
+        elapsedTime = new Date().getTime() - startTime;
+        
+        if(elapsedTime <= allowedTime){
+            if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint){
+                if (distX > 0) {
+                    if (isEnabled) {
+                        previousItem(currentItem);
+                    }
+                } else {
+                    {
+                        if (isEnabled) {
+                            nextItem(currentItem);
+                        }
+                } 
+            }
+        }
+
+        e.preventDefault();
+     });
+}
+
+let el = document.querySelector('.carousel');
+swipedetect(el);
